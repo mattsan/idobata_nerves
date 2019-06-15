a5cde71f9cedda3dd6c9a08ad91ff32fd2f99a33
Eiji MATSUMOTO <e.mattsan@gmail.com>
2019-06-15 17:59:44 +0900

    add messenger app to local

--- local/config/config.exs
+++ local/config/config.exs
@@ -21,6 +21,9 @@ use Mix.Config
 #     config :logger, level: :info
 #
 
+config :messenger,
+  idobata_hook: System.get_env("IDOBATA_HOOK")
+
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
 # by uncommenting the line below and defining dev.exs, test.exs and such.
